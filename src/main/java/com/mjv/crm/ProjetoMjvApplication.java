cb83f1b7a20d180a208a272b372e6256cd0ed475
Jorge Peres <jorgeperes@iMac-de-Jorge.local>
2021-03-20 15:48:01 -0300

    First commit

---
+++ src/main/java/com/mjv/crm/ProjetoMjvApplication.java
@@ -0,0 +1,13 @@
+package com.mjv.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMjvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMjvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjv/crm/ProjetoMjvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjv.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMjvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
